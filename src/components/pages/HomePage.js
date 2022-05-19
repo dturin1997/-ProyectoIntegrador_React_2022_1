14ea376288bb1e4563e976572324100a7141183b
Dario Turin <dturin1997@gmail.com>
2022-05-19 17:17:43 -0500

    Primer commit

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function HomePage() {
+    const items=JSON.parse(localStorage.getItem('user-info'));
     return (
         <div className="text-center">
             <h1 className="main-title home-page-title">welcome to our app</h1>
             <Link to="/">
                 <button className="primary-button">Log out</button>
             </Link>
+            <span><p>{localStorage.getItem('user-info')}</p></span>
+            <span><p>{items.username}</p></span>
+            <span><p>{items.email}</p></span>
         </div>
     )
 }
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,27 +1,73 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom'
 
 import '../../App.css'
 
 export default function SignInPage() {
+
+    const [username,setUsername]=useState("");
+    const [password,setPassword]=useState("");
+
+    const history = useHistory();
+/*
+    useEffect(()=>{
+        if(localStorage.getItem('user-info')){
+            //console.log(localStorage.getItem('user-info'))
+            //history.push("/home")
+        }
+    },[])
+*/
+    async function login(){
+        console.warn(username,password)
+        let item={username,password};
+        let result = await fetch("http://localhost:8080/api/auth/signin",{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json" 
+            },
+            body:JSON.stringify(item)
+        });
+        const data = await result.json();
+        //localStorage.setItem("user-info",JSON.stringify(data))
+        localStorage.setItem("user-info",JSON.stringify(data))
+        console.log(data.id)
+        console.log(data.username)
+        console.log(data.email)
+        history.push("/home")
+    }
+
     return (
         <div className="text-center m-5-auto">
             <h2>Sign in to us</h2>
-            <form action="/home">
-                <p>
-                    <label>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-            </form>
+            <div class="div-form">
+                <div>
+                    <p>
+                        <label>Username or email address</label><br/>
+                        <input type="text" name="first_name" 
+                        onChange={(e)=>setUsername(e.target.value)}
+                        required />
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        <label>Password</label>
+                        <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
+                        <br/>
+                        <input type="password" name="password" 
+                        onChange={(e)=>setPassword(e.target.value)}
+                        required />
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        <button 
+                        onClick={login}
+                        id="sub_btn" type="submit">Login</button>
+                    </p>
+                </div>
+            </div>
             <footer>
                 <p>First time? <Link to="/register">Create an account</Link>.</p>
                 <p><Link to="/">Back to Homepage</Link>.</p>
